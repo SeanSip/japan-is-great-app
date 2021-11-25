e9e7e245978db225b23a6d80628fc72ea24122f9
SeanSip <sean.sipus@gmail.com>
2021-11-25 02:12:30 -0500

    final build

--- src/App.js
+++ src/App.js
@@ -3,38 +3,6 @@ import firebase from './firebase.js';
 import { useEffect, useState } from 'react'
 
 
-/*
-*
-* Explore Japan App
-* -  -  -  -  -  -  -  -  -  -  -  -  -  -  
-*
-* Landing view which would be main app.js (home page) 
-*
-* Region component which will show firebase comments associated with that region 
-*
-* Comment box component that will allow the use to post a comment to the region object in firebase
-*
-* Set this all up for one region and make sure it is working and I am happy with it (and then I can add another region, and so on...)
-*
-* Props (tell components how to change - build a component to respond to the prop)
-*
-*
-* 1st level Homepage is our App.jsx
-* 2nd level call region component 
-*     3rd level comment box component (inside region component)   
-*
-* Homepage : nav & li - also has our landing page img (map of Japan)
-*
-* Region component: call to firebase, loop through that data and display it
-*
-* final component: takes users information into text field, user then submits info with submit button, component posts that information to firebase
-*
-* Stretch Goals:
-*
-* 1st: Set up our region component so that it refreshes itself when new info is added to firebase
-*
-* 2nd: To use props for each component so that I can add different regions (make 1 component that is uses a prop that can then change the context of the region component to be different regions)
-*/
 
 function App() {
   const [selectedRegion, setSelectedRegion] = useState("");
@@ -45,14 +13,14 @@ function App() {
     setSelectedRegion(event.target.value);
   }
 
-  //testing
+  
   const handleSubmit = (event) => {
     event.preventDefault();
     const dbRef = firebase.database().ref();
     dbRef.push(userInput);
     setUserInput('');
   }
-  //testing ^
+  
 
   useEffect(() => {
     const dbRef = firebase.database().ref();
@@ -72,21 +40,14 @@ function App() {
 
   }, [])
   const handleChange = (e) => {
-    // e.target.value 
     setUserInput(e.target.value)
   }
 
 
   const removeComment = (remove) => {
-    // get access to our database
     const dbRef = firebase.database().ref();
-
-    // use a new firebase methods to remove an item
     dbRef.child(remove).remove();
   }
-
-  
-
 
 
 return (
@@ -118,12 +79,14 @@ return (
         <form onSubmit={handleSubmit}>
           <label htmlFor="newComment">Write your comments below:</label>
             <input 
+            required="true"
             placeholder="Please type in your adventure here"
             id="newComment"
             type="text"
             value={ userInput }
             onChange={ handleChange }
             />
+            <button>Submit</button>
           </form>
       <div class="submitContainer">
           <ul class="addedComments">
@@ -131,8 +94,8 @@ return (
               recommendationsList.map(recommendation => {
                 return(
                   <li key={ recommendation.id }>
-                    <button onClick={ () => removeComment(recommendation.id) }>Remove</button>
                     {recommendation.comment}
+                    <button onClick={ () => removeComment(recommendation.id) }>Remove</button>
                   </li>
                 )
               })
@@ -145,5 +108,6 @@ return (
 </>  
   );
 }
+
 
 export default App;
